d5cd5957bed07a8672039bb114fbc9298d72144f
Greg Simon <gregsimon@gmail.com>
2015-09-28 10:13:26 -0700

    initial checkin osx-only version

---
+++ ftk_osx/Flutter ToolKit/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Flutter ToolKit
+//
+//  Created by Greg Simon on 9/23/15.
+//  Copyright © 2015 Greg Simon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
---
+++ ftk_osx/Flutter ToolKitTests/Flutter_ToolKitTests.m
@@ -0,0 +1,39 @@
+//
+//  Flutter_ToolKitTests.m
+//  Flutter ToolKitTests
+//
+//  Created by Greg Simon on 9/23/15.
+//  Copyright © 2015 Greg Simon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Flutter_ToolKitTests : XCTestCase
+
+@end
+
+@implementation Flutter_ToolKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
