ec08d49c7458f6eb954bd61d1d22541392306bae
Greg Simon <gregsimon@gmail.com>
2015-10-21 23:45:39 -0700

    wx version building for win and opening files

--- ftk_wx/main.cpp
+++ ftk_wx/main.cpp
@@ -3,9 +3,22 @@
 
 #include "wx/wxprec.h"
 
+#include <wx/textfile.h>
+#include <wx/textctrl.h>
+#include <wx/stc/stc.h>
+#include <wx/font.h>
+#include <wx/filedlg.h>
+#include <wx/wfstream.h>
+#include <wx/log.h>
+
+
+
 #ifndef WX_PRECOMP
 #include "wx/wx.h"
 #endif
+
+// -----------------------------------------------------------------------
+
 
 class FTKApp : public wxApp
 {
@@ -13,66 +26,124 @@ public:
 	virtual bool OnInit();
 };
 
-class MyFrame : public wxFrame
+// -----------------------------------------------------------------------
+
+class FTKFrame : public wxFrame
 {
 public:
-	MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+	FTKFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+
+	wxStyledTextCtrl *main_edit_box_;
+
 private:
-	void OnHello(wxCommandEvent& event);
+	void OnOpen(wxCommandEvent& event);
 	void OnExit(wxCommandEvent& event);
+	void OnDebugStart(wxCommandEvent& event);
 	void OnAbout(wxCommandEvent& event);
 	wxDECLARE_EVENT_TABLE();
 };
 
 enum
 {
-	ID_Hello = 1
+	MENU_DebugStart= wxID_HIGHEST + 1,
+	MENU_DebugStop,
+	TEXT_Main,
+	ID_LAST
 };
 
-wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-EVT_MENU(ID_Hello, MyFrame::OnHello)
-EVT_MENU(wxID_EXIT, MyFrame::OnExit)
-EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
+wxBEGIN_EVENT_TABLE(FTKFrame, wxFrame)
+EVT_MENU(wxID_OPEN, FTKFrame::OnOpen)
+EVT_MENU(wxID_EXIT, FTKFrame::OnExit)
+EVT_MENU(wxID_ABOUT, FTKFrame::OnAbout)
+EVT_MENU(MENU_DebugStart, FTKFrame::OnDebugStart)
 wxEND_EVENT_TABLE()
 wxIMPLEMENT_APP(FTKApp);
 
-bool MyApp::OnInit()
+bool FTKApp::OnInit()
 {
-	MyFrame *frame = new MyFrame("FTK", wxPoint(50, 50), wxSize(450, 340));
+	wxLog::SetActiveTarget(new wxLogStderr());
+
+	FTKFrame *frame = new FTKFrame("Flutter ToolKit", wxPoint(50, 50), wxSize(1024, 768));
 	frame->Show(true);
 	return true;
 }
 
-MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+FTKFrame::FTKFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
 	: wxFrame(NULL, wxID_ANY, title, pos, size)
 {
 	wxMenu *menuFile = new wxMenu;
-	menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-		"Help string shown in status bar for this menu item");
+	menuFile->Append(wxID_OPEN, "&Open...\tCtrl-O",
+		"Open file");
 	menuFile->AppendSeparator();
 	menuFile->Append(wxID_EXIT);
+
+	wxMenu* menuDebug = new wxMenu;
+	menuDebug->Append(MENU_DebugStart, "Start Debugging\tF5",
+		"Start debugging this project");
+
 	wxMenu *menuHelp = new wxMenu;
 	menuHelp->Append(wxID_ABOUT);
 	wxMenuBar *menuBar = new wxMenuBar;
 	menuBar->Append(menuFile, "&File");
+	menuBar->Append(menuDebug, "&Debug");
 	menuBar->Append(menuHelp, "&Help");
 	SetMenuBar(menuBar);
+
 	CreateStatusBar();
-	SetStatusText("Welcome to wxWidgets!");
+	SetStatusText("Welcome to Flutter ToolKit!");
+
+	main_edit_box_ = new wxStyledTextCtrl(this, TEXT_Main); 
+	/*
+	wxFont* font = new wxFont(10, wxFONTFAMILY_TELETYPE, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL, false, "Consolas");
+	main_edit_box_->SetFont(*font);
+	main_edit_box_->SetBackgroundColour(wxColour(30, 30, 30));
+	main_edit_box_->SetForegroundColour(wxColour(200, 200, 200));
+	*/
 }
 
-void MyFrame::OnExit(wxCommandEvent& event)
+void FTKFrame::OnExit(wxCommandEvent& event)
 {
 	Close(true);
 }
 
-void MyFrame::OnAbout(wxCommandEvent& event)
+void FTKFrame::OnAbout(wxCommandEvent& event)
 {
 	wxMessageBox("This is a wxWidgets' Hello world sample",
 		"About Hello World", wxOK | wxICON_INFORMATION);
 }
 
-void MyFrame::OnHello(wxCommandEvent& event)
+void FTKFrame::OnOpen(wxCommandEvent& event)
 {
-	wxLogMessage("Hello world from wxWidgets!");
+	wxFileDialog
+		openFileDialog(this, _("Open file"), "", "",
+			"Dart files (*.dart)|*.dart|CoffeeScript files (*.coffee)|*.coffee|JavaScript files (*.js)|*.js", wxFD_OPEN | wxFD_FILE_MUST_EXIST);
+	if (openFileDialog.ShowModal() == wxID_CANCEL)
+		return;     // the user changed idea...
+
+
+	wxString        complete_file_contents;
+	wxString        str;
+	wxTextFile      tfile;
+	if (!tfile.Open(openFileDialog.GetPath())) {
+		wxLogError("Cannot open file '%s'.", openFileDialog.GetPath());
+		return;
+	}
+
+	// read the first line
+	str = tfile.GetFirstLine();
+	complete_file_contents += str;
+	while (!tfile.Eof())
+	{
+		str = tfile.GetNextLine();
+		complete_file_contents += "\n";
+		complete_file_contents += str;
+	}
+
+	wxLogMessage("File loaded is\n%s\n", complete_file_contents);
+	main_edit_box_->SetValue(complete_file_contents);
+}
+
+void FTKFrame::OnDebugStart(wxCommandEvent& event)
+{
+
 }
