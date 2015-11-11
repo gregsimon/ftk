e82e9679b517c694ab0b7d8ca142f288422f5c10
Greg Simon <gregsimon@gmail.com>
2015-11-10 21:33:29 -0800

    remove built-in font, we'll use the systems font

--- ftk_wx/ftk_platform_win.cpp
+++ ftk_wx/ftk_platform_win.cpp
@@ -5,41 +5,8 @@
 #include <tchar.h>
 
 
-#include "ftk_resource.h"
-
-int CALLBACK fontcallback(
-	const LOGFONT    *lpelfe,
-	const TEXTMETRIC *lpntme,
-	DWORD      FontType,
-	LPARAM     lParam
-	);
-
 int FTK_Platform_Init()
 {
-	// Make our custom Inconsolata.otf font available to load. In Windows
-	// we can pull the binary .otf file out of a resource, map it and 
-	// add it to the system where only our process can "see" it.
-	HRSRC hrc = FindResource(GetModuleHandle(NULL), MAKEINTRESOURCE(IDR_PROGFONT), (RT_RCDATA));
-	if (!hrc)
-		return -1;
-
-	HGLOBAL fhandle = LoadResource(0, hrc);
-	if (!fhandle)
-		return -2;
-
-
-	DWORD font_size = SizeofResource(0, hrc);
-	VOID* font_data = LockResource(fhandle);
-	if (!font_data)
-		return -3;
-
-	DWORD  num_installed = 0;
-	HANDLE h_font = AddFontMemResourceEx(font_data, font_size, 0, &num_installed);
-	if (!num_installed)
-		return -4;
-
-	// TODO : we are leaking these handles right now
-
 	return 0;
 }
 
